edbf3e94b4702b131d256fcb85321a2452d28314
vuquangkhtn <vuquangkhtn@gmail.com>
2017-02-15 16:28:06 +0700

    PrintScreen
    Print your screen each 2 sec

---
+++ Source code/PrintScreen/PrintScreen.cpp
@@ -0,0 +1,473 @@
+﻿// PrintScreen.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "PrintScreen.h"
+#include <Windowsx.h>
+#include <winuser.h> 
+#include <direct.h>
+#include <commctrl.h>
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(lib, "ComCtl32.lib")
+
+#define MAX_LOADSTRING 100
+#define VK_S 0x53
+#define IDT_TIMER1 3000
+#define TIME_ELAPSE 2000
+
+HWND status;
+bool isRunning = false;
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+void doStart(HWND hWnd);
+void doRemove(HWND hWnd);
+void CreateBMPFile(HWND hwnd, LPTSTR pszFile, PBITMAPINFO pbi, HBITMAP hBMP, HDC hDC);
+PBITMAPINFO CreateBitmapInfoStruct(HWND hwnd, HBITMAP hBmp);
+void GetScreenShot(HWND hWnd);
+
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_PRINTSCREEN, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PRINTSCREEN));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PRINTSCREEN));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_PRINTSCREEN);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // Store instance handle in our global variable
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_CREATE:
+		{
+			LOGFONT lf;
+			GetObject(GetStockObject(DEFAULT_GUI_FONT), sizeof(LOGFONT), &lf);
+			HFONT hFont = CreateFont(20, lf.lfWidth,
+				lf.lfEscapement, lf.lfOrientation, lf.lfWeight,
+				lf.lfItalic, lf.lfUnderline, lf.lfStrikeOut, lf.lfCharSet,
+				lf.lfOutPrecision, lf.lfClipPrecision, lf.lfQuality,
+				lf.lfPitchAndFamily, lf.lfFaceName);
+			status = CreateWindowEx(0, L"STATIC", L"Press Shift+Ctrl+Alt+S to start!", WS_CHILD | WS_VISIBLE | SS_CENTER, 0, 0, 220, 20, hWnd, NULL, hInst, NULL);
+			SendMessage(status, WM_SETFONT, WPARAM(hFont), TRUE);
+			SetFocus(hWnd);
+			_mkdir("images");
+		}
+		break;
+	case WM_KEYDOWN:
+		if (GetAsyncKeyState(VK_CONTROL) != 0 && GetAsyncKeyState(VK_SHIFT) != 0  && GetAsyncKeyState(VK_MENU) != 0 && GetAsyncKeyState(VK_S) != 0)
+		{
+			doStart(hWnd);
+		}
+		break;
+	case WM_CTLCOLORSTATIC:
+	{
+		HDC hdcStatic = (HDC) wParam;
+		SetTextColor(hdcStatic, RGB(0,0,0));
+		SetBkColor(hdcStatic, RGB(255,255,255));
+		return (INT_PTR)CreateSolidBrush(RGB(255,255,255));
+	}
+	case WM_TIMER:
+		switch(wParam)
+		{
+		case IDT_TIMER1:
+			if(isRunning)
+			{
+				GetScreenShot(hWnd);
+			}
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case ID_REMOVE:
+			doRemove(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		KillTimer(hWnd,IDT_TIMER1);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+void doStart(HWND hWnd)
+{
+	if(isRunning)
+		return;
+	if (OpenClipboard(hWnd))
+	{
+		EmptyClipboard();
+	}
+	CloseClipboard();
+	MessageBox(NULL,L"Start successfully!",L"Started",MB_OK);
+	SetWindowText(status,L"Loading...");
+	isRunning = true;
+	SetTimer(hWnd,IDT_TIMER1,TIME_ELAPSE,(TIMERPROC)NULL);
+	return;
+}
+
+void doRemove(HWND hWnd)
+{
+	if(!isRunning)
+		return;
+	isRunning = false;
+	MessageBox(NULL,L"Stop successfully!",L"Stopped",MB_OK);
+	SetWindowText(status,L"Stop successfully!");
+}
+
+void GetScreenShot(HWND hWnd)
+{
+    int x1, y1, x2, y2, w, h;
+
+    // get screen dimensions
+    x1  = GetSystemMetrics(SM_XVIRTUALSCREEN);
+    y1  = GetSystemMetrics(SM_YVIRTUALSCREEN);
+    x2  = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    y2  = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    w   = x2 - x1;
+    h   = y2 - y1;
+
+    // copy screen to bitmap
+    HDC     hScreen = GetDC(NULL);
+    HDC     hDC     = CreateCompatibleDC(hScreen);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, w, h);
+    HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
+    BOOL    bRet    = BitBlt(hDC, 0, 0, w, h, hScreen, x1, y1, SRCCOPY);
+
+	SYSTEMTIME time;
+	WCHAR* path = new WCHAR[50];
+	GetLocalTime(&time);
+	swprintf(path,50,L"images\\Screen_%d-%d-%d_%dh%dm%ds.bmp",time.wYear,time.wMonth,time.wDay,time.wHour,time.wMinute,time.wSecond);
+	CreateBMPFile(hWnd,path,CreateBitmapInfoStruct(hWnd,hBitmap),hBitmap,hDC);
+
+	//in ảnh ra màn hình
+	BITMAP bitmap;
+	GetObject(hBitmap, sizeof(BITMAP), &bitmap);
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	int height = rect.bottom - rect.top;
+	int width = height * 16 / 9; // Duy trì tỉ lệ 16:9
+	HDC hdc = GetDC(hWnd);
+	StretchBlt(hdc, 0, 0, width, height,
+		hDC, 0, 0, bitmap.bmWidth, bitmap.bmHeight, SRCCOPY);
+
+    // save bitmap to clipboard
+    OpenClipboard(NULL);
+    EmptyClipboard();
+    SetClipboardData(CF_BITMAP, hBitmap);
+    CloseClipboard();   
+
+    // clean up
+    SelectObject(hDC, old_obj);
+    DeleteDC(hDC);
+    ReleaseDC(NULL, hScreen);
+    DeleteObject(hBitmap);
+	return;
+}
+
+//Tạo file bmp: nguồn microsoft
+void CreateBMPFile(HWND hwnd, LPTSTR pszFile, PBITMAPINFO pbi, 
+                  HBITMAP hBMP, HDC hDC) 
+ { 
+     HANDLE hf;                 // file handle  
+    BITMAPFILEHEADER hdr;       // bitmap file-header  
+    PBITMAPINFOHEADER pbih;     // bitmap info-header  
+    LPBYTE lpBits;              // memory pointer  
+    DWORD dwTotal = 0;              // total count of bytes  
+    DWORD cb = 0;                   // incremental count of bytes  
+    BYTE *hp;                   // byte pointer  
+    DWORD dwTmp; 
+
+    pbih = (PBITMAPINFOHEADER) pbi; 
+    lpBits = (LPBYTE) GlobalAlloc(GMEM_FIXED, pbih->biSizeImage);
+
+    if (!lpBits) 
+	{
+         //errhandler("GlobalAlloc", hwnd); 
+	}
+
+    // Retrieve the color table (RGBQUAD array) and the bits  
+    // (array of palette indices) from the DIB.  
+    if (!GetDIBits(hDC, hBMP, 0, (WORD) pbih->biHeight, lpBits, pbi, 
+        DIB_RGB_COLORS)) 
+    {
+        //errhandler("GetDIBits", hwnd); 
+    }
+
+    // Create the .BMP file.  
+    hf = CreateFile(pszFile, 
+                   GENERIC_READ | GENERIC_WRITE, 
+                   (DWORD) 0, 
+                    NULL, 
+                   CREATE_ALWAYS, 
+                   FILE_ATTRIBUTE_NORMAL, 
+                   (HANDLE) NULL); 
+    if (hf == INVALID_HANDLE_VALUE) 
+	{
+        //errhandler("CreateFile", hwnd); 
+	}
+    hdr.bfType = 0x4d42;        // 0x42 = "B" 0x4d = "M"  
+    // Compute the size of the entire file.  
+    hdr.bfSize = (DWORD) (sizeof(BITMAPFILEHEADER) + 
+                 pbih->biSize + pbih->biClrUsed 
+                 * sizeof(RGBQUAD) + pbih->biSizeImage); 
+    hdr.bfReserved1 = 0; 
+    hdr.bfReserved2 = 0; 
+
+    // Compute the offset to the array of color indices.  
+    hdr.bfOffBits = (DWORD) sizeof(BITMAPFILEHEADER) + 
+                    pbih->biSize + pbih->biClrUsed 
+                    * sizeof (RGBQUAD); 
+
+    // Copy the BITMAPFILEHEADER into the .BMP file.  
+    if (!WriteFile(hf, (LPVOID) &hdr, sizeof(BITMAPFILEHEADER), 
+        (LPDWORD) &dwTmp,  NULL)) 
+    {
+       //errhandler("WriteFile", hwnd); 
+    }
+
+    // Copy the BITMAPINFOHEADER and RGBQUAD array into the file.  
+    if (!WriteFile(hf, (LPVOID) pbih, sizeof(BITMAPINFOHEADER) 
+                  + pbih->biClrUsed * sizeof (RGBQUAD), 
+                  (LPDWORD) &dwTmp, ( NULL)))
+	{
+        //errhandler("WriteFile", hwnd); 
+	}
+    // Copy the array of color indices into the .BMP file.  
+    dwTotal = cb = pbih->biSizeImage; 
+    hp = lpBits; 
+    if (!WriteFile(hf, (LPSTR) hp, (int) cb, (LPDWORD) &dwTmp,NULL)) 
+	{
+           //errhandler("WriteFile", hwnd); 
+	}
+
+    // Close the .BMP file.  
+     if (!CloseHandle(hf)) 
+	 {
+          // errhandler("CloseHandle", hwnd); 
+	 }
+
+    // Free memory.  
+    GlobalFree((HGLOBAL)lpBits);
+}
+
+PBITMAPINFO CreateBitmapInfoStruct(HWND hwnd, HBITMAP hBmp)
+{ 
+    BITMAP bmp; 
+    PBITMAPINFO pbmi; 
+    WORD    cClrBits = 0; 
+
+    // Retrieve the bitmap color format, width, and height.  
+    if (!GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp)) 
+	{
+        //errhandler("GetObject", hwnd); 
+	}
+
+    // Convert the color format to a count of bits.  
+    cClrBits = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel); 
+    if (cClrBits == 1) 
+        cClrBits = 1; 
+    else if (cClrBits <= 4) 
+        cClrBits = 4; 
+    else if (cClrBits <= 8) 
+        cClrBits = 8; 
+    else if (cClrBits <= 16) 
+        cClrBits = 16; 
+    else if (cClrBits <= 24) 
+        cClrBits = 24; 
+    else cClrBits = 32; 
+
+    // Allocate memory for the BITMAPINFO structure. (This structure  
+    // contains a BITMAPINFOHEADER structure and an array of RGBQUAD  
+    // data structures.)  
+
+     if (cClrBits < 24) 
+         pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+                    sizeof(BITMAPINFOHEADER) + 
+                    sizeof(RGBQUAD) * (1<< cClrBits)); 
+
+     // There is no RGBQUAD array for these formats: 24-bit-per-pixel or 32-bit-per-pixel 
+
+     else 
+         pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+                    sizeof(BITMAPINFOHEADER)); 
+
+    // Initialize the fields in the BITMAPINFO structure.  
+
+    pbmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER); 
+    pbmi->bmiHeader.biWidth = bmp.bmWidth; 
+    pbmi->bmiHeader.biHeight = bmp.bmHeight; 
+    pbmi->bmiHeader.biPlanes = bmp.bmPlanes; 
+    pbmi->bmiHeader.biBitCount = bmp.bmBitsPixel; 
+    if (cClrBits < 24) 
+        pbmi->bmiHeader.biClrUsed = (1<<cClrBits); 
+
+    // If the bitmap is not compressed, set the BI_RGB flag.  
+    pbmi->bmiHeader.biCompression = BI_RGB; 
+
+    // Compute the number of bytes in the array of color  
+    // indices and store the result in biSizeImage.  
+    // The width must be DWORD aligned unless the bitmap is RLE 
+    // compressed. 
+    pbmi->bmiHeader.biSizeImage = ((pbmi->bmiHeader.biWidth * cClrBits +31) & ~31) /8
+                                  * pbmi->bmiHeader.biHeight; 
+    // Set biClrImportant to 0, indicating that all of the  
+    // device colors are important.  
+     pbmi->bmiHeader.biClrImportant = 0; 
+     return pbmi; 
+ } 
---
+++ readme.txt
@@ -0,0 +1,26 @@
+*My ID:		1412430
+*My Name:	Nguyen Vu Quang
+*My email:	vuquangkhtn@gmail.com
+
+/////////////////////////////////////////////////////////
+===What you have done?====
+
+ScreenWatch. 
+Write a program to periodically every 2 second save a picture of the screen to disk. Activate with Ctrl + Alt + Shift + S.
+Filename be in this format: "Screen_yy-mm-dd hhmmss.bmp".
+
+
+/////////////////////////////////////////////////////////
+===Main flow===
+Press Ctrl + Alt + Shift + S. Periodically every 2 second save a picture of the screen to "images" folder.
+
+
+===Additional flow===
+
+Click "Stop" to pause saving a picture of the screen.
+Press Ctrl + Alt + Shift + S to start again.
+
+
+/////////////////////////////////////////////////////////
+===Link record===
+https://youtu.be/_BvefsiKr6Q
